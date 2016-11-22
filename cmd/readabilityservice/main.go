4a90b984f84aa931d0622f7530280f1f812426f9
Johann Höchtl <johann.hoechtl@gmail.com>
2016-11-22 10:53:52 +0100

    add readability service to be used for adequate project

--- cmd/readabilityservice/main.go
+++ cmd/readabilityservice/main.go
@@ -8,18 +8,35 @@ import (
 
 	restful "github.com/emicklei/go-restful"
 	"github.com/emicklei/go-restful/swagger"
+	"github.com/the42/adequate/portalwatch"
 	"github.com/the42/readability"
 )
 
-type ReadabiltyRequest struct {
+type ReadabilityRequest struct {
 	CheckString     *string `description:"Input String whose readability should be checked"`
 	CorrelationID   *string `description:"request provided CorrelationID copied to response for requests/response matchmaking"`
 	ReadabilityType *string `description:"Algorithm to use for readability check"`
 }
+
 type ReadabilityResponse struct {
-	ReadabiltyRequest `description:"Copied to response from Request without CheckString"`
-	Response          struct {
+	ReadabilityRequest ReadabilityRequest
+	Response           struct {
 		Readability float32 `description:"Readability score result"`
+		Message     *string `description:"diagnostic message returned by readability ccheck"`
+		StatusCode  int     `description:"0:success, -1: no success, check Message"`
+	}
+}
+type PortalReadabilityRequest struct {
+	CKANMDAustria   *portalwatch.CKANMDAustria `description:"the raw CKAN metadata harvested"`
+	CorrelationID   *string                    `description:"request provided CorrelationID copied to response for requests/response matchmaking"`
+	ReadabilityType *string                    `description:"Algorithm to use for readability check"`
+}
+
+type PortalReadabilityResponse struct {
+	PortalReadabilityRequest PortalReadabilityRequest `description:"Copied to response from Request`
+	Response                 struct {
+		Readability float32 `description:"Readability score result"`
+		CheckString *string `description:"The actual tested string"`
 		Message     *string `description:"diagnostic message returned by readability ccheck"`
 		StatusCode  int     `description:"0:success, -1: no success, check Message"`
 	}
@@ -36,9 +53,83 @@ type readabilityservice struct {
 	r *readability.Readability
 }
 
+func appendclosingiandblank(in string) string {
+	if len(in) > 0 {
+		last_char := in[len(in)-1]
+		if !(last_char == '.' || last_char == '!' || last_char == '?') {
+			return in + ". "
+		}
+		return in + " "
+	}
+	return in
+}
+
+func (s *readabilityservice) portalreadabilityservice(request *restful.Request, response *restful.Response) {
+
+	readabilityrequest := PortalReadabilityRequest{}
+	if err := request.ReadEntity(&readabilityrequest); err != nil {
+		logresponse(response, http.StatusBadRequest, fmt.Sprintf("unable to parse request: %s", err.Error()))
+		return
+	}
+
+	if readabilityrequest.CKANMDAustria == nil {
+		logresponse(response, http.StatusBadRequest, fmt.Sprintf("PortalReadabilityRequest.CKANMDAustriaportal is required but not set"))
+		return
+	}
+
+	result := PortalReadabilityResponse{PortalReadabilityRequest: readabilityrequest}
+	// set the input struct to nil for performance reasons
+	result.PortalReadabilityRequest.CKANMDAustria = nil
+
+	var readability_type readability.CompareType
+	if readabilityrequest.ReadabilityType != nil && len(*readabilityrequest.ReadabilityType) > 0 {
+		readability_type = readabilityrequesttypemappings[*readabilityrequest.ReadabilityType]
+	} else {
+		readability_type = readability.WSTF1
+	}
+
+	// prepare input data for readability check
+	// The algorithm is as follows:
+	//   if the notes - field doesn't end with a '.', add one (WSTF operates on the notion of a "sentence")
+	//   if the title - field doesn't end with a '.', add one (WSTF operates on the notion of a "sentence")
+	// The resulting readability is performed as the WSTF on "notes + title + tags" (in this order).
+
+	var readability_inputstring string
+	if readabilityrequest.CKANMDAustria.Notes != nil {
+		readability_inputstring = *readabilityrequest.CKANMDAustria.Notes
+		readability_inputstring = appendclosingiandblank(readability_inputstring)
+	}
+	if readabilityrequest.CKANMDAustria.Title != nil {
+		readability_inputstring += *readabilityrequest.CKANMDAustria.Title
+		readability_inputstring = appendclosingiandblank(readability_inputstring)
+	}
+	for _, v := range readabilityrequest.CKANMDAustria.Tags {
+		if dn := v.DisplayName; dn != nil {
+			readability_inputstring += " " + *dn
+		}
+	}
+
+	result.Response.CheckString = &readability_inputstring
+
+	switch readability_type {
+	case readability.WSTF1, readability.WSTF2, readability.WSTF3, readability.WSTF4:
+		readabilityresult, err := s.r.WienerSachTextFormelType(readability_inputstring, readability_type)
+		if err != nil {
+			logresponse(response, http.StatusBadRequest, fmt.Sprintf("WienerSachTextFormelType returned error: %s", err.Error()))
+			return
+		}
+		result.Response.Readability = readabilityresult
+	default:
+		result.Response.StatusCode = -1
+		s := "no method found to perform readability check"
+		result.Response.Message = &s
+	}
+	response.WriteAsJson(result)
+}
+
 func (s *readabilityservice) readabilityservice(request *restful.Request, response *restful.Response) {
 
-	readabilityrequest := ReadabiltyRequest{}
+	readabilityrequest := ReadabilityRequest{}
 	if err := request.ReadEntity(&readabilityrequest); err != nil {
 		logresponse(response, http.StatusBadRequest, fmt.Sprintf("unable to parse request: %s", err.Error()))
 		return
@@ -56,9 +147,9 @@ func (s *readabilityservice) readabilityservice(request *restful.Request, respon
 		readability_type = readability.WSTF1
 	}
 
-	result := ReadabilityResponse{ReadabiltyRequest: readabilityrequest}
+	result := ReadabilityResponse{ReadabilityRequest: readabilityrequest}
 	// set the input string to nil for performance reasons. May correlate result to request by using CorrelationID
-	result.ReadabiltyRequest.CheckString = nil
+	result.ReadabilityRequest.CheckString = nil
 
 	switch readability_type {
 	case readability.WSTF1, readability.WSTF2, readability.WSTF3, readability.WSTF4:
@@ -75,7 +166,6 @@ func (s *readabilityservice) readabilityservice(request *restful.Request, respon
 	}
 	response.WriteAsJson(result)
 }
-
 func logresponse(resp *restful.Response, code int, message string) {
 	resp.WriteErrorString(code, message)
 	log.Print(message)
@@ -86,7 +176,7 @@ func main() {
 		Produces(restful.MIME_JSON).
 		Consumes(restful.MIME_JSON)
 
-		//BEGIN: CORS support
+	//BEGIN: CORS support
 	if enable_cors := os.Getenv("ENABLE_CORS"); enable_cors != "" {
 		cors := restful.CrossOriginResourceSharing{
 			ExposeHeaders:  []string{"X-My-Header"},
@@ -113,10 +203,20 @@ func main() {
 		To(s.readabilityservice).
 		Produces(restful.MIME_JSON).
 		Consumes(restful.MIME_JSON).
-		Reads(ReadabiltyRequest{}).
-		Returns(http.StatusOK, "", ReadabilityResponse{}).
-		Returns(http.StatusInternalServerError, "", nil).
-		Returns(http.StatusBadRequest, "", nil))
+		Doc("performs readability checks on an input string").
+		Reads(ReadabilityRequest{}).
+		Returns(http.StatusOK, "success", ReadabilityResponse{}).
+		Returns(http.StatusInternalServerError, "failure", nil).
+		Returns(http.StatusBadRequest, "failure", nil))
+	ws.Route(ws.PUT("/portalreadability").
+		To(s.portalreadabilityservice).
+		Produces(restful.MIME_JSON).
+		Consumes(restful.MIME_JSON).
+		Doc("performs a readability check based on the CKAN AT Open Data Metadata scheme https://www.ref.gv.at/Veroeffentlichte-Informationen.2774.0.html . Only the fields description (ID=9, CKAN \"notes\"), title (ID=8, CKAN \"title\") and keywords (ID=11, CKAN \"Tags\") are used for the check").
+		Reads(PortalReadabilityRequest{}).
+		Returns(http.StatusOK, "success", PortalReadabilityResponse{}).
+		Returns(http.StatusInternalServerError, "failure", nil).
+		Returns(http.StatusBadRequest, "failure", nil))
 	restful.Add(ws)
 
 	port := os.Getenv("PORT")
